8a7f8e8cfb0f7dbbdf000fc02a61483fdc155f2b
ram8528 <106538883+ram8528@users.noreply.github.com>
2024-04-18 08:55:57 +0530

    Day3 Just React JSX FUnctional

--- App.js
+++ App.js
@@ -1,41 +1,71 @@
-// Manipulate the HTML DOM using Javscript
-// const heading = document.createElement("h1");
-// heading.innerHTML = "Namaste Everyone";
-// const root = document.getElementById("root");
-// root.appendChild(heading);
-
-// Manipulate the HTML DOM using React
-
-// Create nested React Elements
-
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading  =  React.createElement(
-    "h1",
-    {
-        id:"title1",
-        // style:{
-        //     background:"cyan",
-        // },
-        className: "title1"
-    },
-    "Heading Content is Here"
+/*const heading = React.createElement(
+  "h1",
+  {
+    id: "heading",
+  },
+  "This is React Lecture 3🚀🚀🚀🚀🚀🚀🚀🚀"
 );
 
-const paragraph = React.createElement(
-    "p",
-    {
-        id: "para1"
-    },
-    "Hii This is Ramdeep Kesharwani 🔥🚀🚀. I am a Passionate Full Stack Web Developer🫤 as well as Software Developer"
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+*/
 
-const root1 = ReactDOM.createRoot(document.getElementById("root1"));
-root1.render(heading);
+/// Now this is a JSX Code While the above one is purely React
 
-const root2 = ReactDOM.createRoot(document.getElementById("root2"));
-root2.render(paragraph);
+//const jsxHeading = <h1 id="heading">This is React Lecture 3🚀🚀🚀🚀🚀🚀🚀🚀</h1>;
+const jsxHeading = (
+  <h1 className="heading">This is React Lecture 3🚀🚀🚀🚀🚀🚀🚀🚀</h1>
+);
+// console.log(jsxHeading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxHeading);
 
 
+//            -----   React Functional Component
+
+const HeadingComponent = () => {
+    return (<h1>Ramdeep Kesharwani😍😍😍</h1>);
+};
+
+// root.render(<HeadingComponent/>);
+
+//          ------ JSX with Normal JS FUnction e\without arrow function but this is not a good approach
+
+const tit = (
+    <h1>Ram9395</h1>
+);
+
+const Title = function(){
+    return (
+        
+        <div>{tit}
+    <h1 className="head">Ramdeep Kesharwani8528397877</h1>
+    <HeadingComponent/>             
+    </div>
+    // In this code we have done 3 task
+    // 1. We have invoked a function inside jsx TItle
+    // 2. We have given a h1 tag inside div from our Side
+    // 3. RFC inside RFC
+);
+};
+
+// root.render(<Title/>);
+
+const num = 1000;
+const NewComponent = () =>{
+    return (
+    <div id="container"> 
+        {/* <NewComponent/> */}
+        <h2>{num}</h2>
+        <h3>{290+98-100}</h3>
+        <h1>Ramdeep is Here Welcome Coders You are with a billionare</h1>
+        
+    </div>
+    );
+};
+
+root.render(<NewComponent/>);
