95f1904ff87bd2b4a25ab456a2aa70a115db2da9
ram8528 <106538883+ram8528@users.noreply.github.com>
2024-04-21 17:46:50 +0530

    NOW This Amazing Code

--- App.js
+++ App.js
@@ -24,48 +24,44 @@ const jsxHeading = (
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(jsxHeading);
 
-
 //            -----   React Functional Component
 
 const HeadingComponent = () => {
-    return (<h1>Ramdeep Kesharwani😍😍😍</h1>);
+  return <h1>Ramdeep Kesharwani😍😍😍</h1>;
 };
 
 // root.render(<HeadingComponent/>);
 
 //          ------ JSX with Normal JS FUnction e\without arrow function but this is not a good approach
 
-const tit = (
-    <h1>Ram9395</h1>
-);
+const tit = <h1>Ram9395</h1>;
 
-const Title = function(){
-    return (
-        
-        <div>{tit}
-    <h1 className="head">Ramdeep Kesharwani8528397877</h1>
-    <HeadingComponent/>             
+const Title = function () {
+  return (
+    <div>
+      {tit}
+      <h1 className="head">Ramdeep Kesharwani8528397877</h1>
+      <HeadingComponent />
     </div>
     // In this code we have done 3 task
     // 1. We have invoked a function inside jsx TItle
     // 2. We have given a h1 tag inside div from our Side
     // 3. RFC inside RFC
-);
+  );
 };
 
 // root.render(<Title/>);
 
 const num = 1000;
-const NewComponent = () =>{
-    return (
-    <div id="container"> 
-        {/* <NewComponent/> */}
-        <h2>{num}</h2>
-        <h3>{290+98-100}</h3>
-        <h1>Ramdeep is Here Welcome Coders You are with a billionare</h1>
-        
+const NewComponent = () => {
+  return (
+    <div id="container">
+      {/* <NewComponent/> */}
+      <h2>{num}</h2>
+      <h3>{290 + 98 - 100}</h3>
+      <h1>Ramdeep is Here Welcome Coders You are with a billionare</h1>
     </div>
-    );
+  );
 };
 
-root.render(<NewComponent/>);
+root.render(<NewComponent />);
